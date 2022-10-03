3828511f38a16e1b6aaf459ff6c6279201118da7
Konstantine <Konstaphy@gmail.com>
2022-10-03 23:26:26 +0500

    Добавил фсдшные директории и канвас

---
+++ src/app/index.tsx
@@ -0,0 +1,10 @@
+import {IritRtf} from "../pages/universities/irit-rtf";
+
+/**
+ * Основная компонента приложения
+ * todo: менюшка и роутер
+ * @mock: Поставил ирит ртф для проверки канваса для себя
+ * */
+export function App(): JSX.Element {
+    return <IritRtf />
+}
--- src/index.tsx
+++ src/index.tsx
@@ -1,11 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import {App} from "./app";
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
 root.render(
   <React.StrictMode>
-    <div></div>
+    <App />
   </React.StrictMode>
 );
