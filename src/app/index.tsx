2fb725fc7e6acc02f03f583e499db1a33a1aa531
lenaa1 <85735585+lenaa1@users.noreply.github.com>
2022-10-05 00:46:12 +0500

    Появился поиск ура!

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -2,11 +2,11 @@ import React from 'react'
 import { Routes, Route } from 'react-router-dom'
 import { Header } from '../widgets/header/ui/header'
 import { Main } from '../pages/main/main'
+import { IritRtf } from '../pages/universities/irit-rtf'
 
 /**
  * Основная компонента приложения
- * todo: менюшка и роутер
- * @mock: Поставил ирит ртф для проверки канваса для себя
+ * todo: менюшка
  * */
 export const App: React.FC = () => {
   return (
@@ -14,6 +14,7 @@ export const App: React.FC = () => {
       <Header />
       <Routes>
         <Route path='/' element={<Main />} />
+        <Route path='/irit-rtf' element={<IritRtf />} />
       </Routes>
     </>
   )
--- src/widgets/header/ui/header.tsx
+++ src/widgets/header/ui/header.tsx
@@ -1,5 +1,13 @@
 import './header.css'
+import Lens from './../../../shared/assets/Vector.svg'
 
 export function Header() {
-  return <div className='header'>g</div>
+  return (
+    <div className='header'>
+      <div className='header__searchbar'>
+        <img className='lens' src={Lens} alt='' />
+        <input className='search' type='search' placeholder='Аудитория или название места' />
+      </div>
+    </div>
+  )
 }
