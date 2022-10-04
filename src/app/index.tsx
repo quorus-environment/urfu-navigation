ef50ca6ccc67a9dbea67a472264fb264c4ace195
lenaa1 <85735585+lenaa1@users.noreply.github.com>
2022-10-05 01:12:04 +0500

    Жестко сверстала разметку основной страницы

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -3,6 +3,7 @@ import { Routes, Route } from 'react-router-dom'
 import { Header } from '../widgets/header/ui/header'
 import { Main } from '../pages/main/main'
 import { IritRtf } from '../pages/universities/irit-rtf'
+import './app.css'
 
 /**
  * Основная компонента приложения
@@ -10,12 +11,12 @@ import { IritRtf } from '../pages/universities/irit-rtf'
  * */
 export const App: React.FC = () => {
   return (
-    <>
+    <div className='app'>
       <Header />
       <Routes>
         <Route path='/' element={<Main />} />
         <Route path='/irit-rtf' element={<IritRtf />} />
       </Routes>
-    </>
+    </div>
   )
 }
--- src/pages/main/main.tsx
+++ src/pages/main/main.tsx
@@ -1,3 +1,14 @@
+import Map from './../../shared/assets/map.jpeg'
+import './main.css'
+
 export function Main() {
-  return <></>
+  return (
+    <section className='main'>
+      <img className='map' src={Map} alt='' />
+      <div>
+        <h2>Институты</h2>
+        <div className='institutes'></div>
+      </div>
+    </section>
+  )
 }
