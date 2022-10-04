81c281cacef1609883f8594aead80add0ae3e747
lenaa1 <85735585+lenaa1@users.noreply.github.com>
2022-10-04 21:16:00 +0500

    добавила Header и роутинг

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,10 +1,19 @@
-import {IritRtf} from "../pages/universities/irit-rtf";
+import React from "react";
+import {Routes, Route} from 'react-router-dom';
+import { Header } from "../widgets/header/ui/header";
+import {Main} from "../pages/main/main";
 
 /**
  * Основная компонента приложения
  * todo: менюшка и роутер
  * @mock: Поставил ирит ртф для проверки канваса для себя
  * */
-export function App(): JSX.Element {
-    return <IritRtf />
+export const App: React.FC = () => {
+    return(<>
+        < Header />
+        <Routes>
+            <Route path="/" element={< Main/>}/>
+        </Routes>
+        </>
+    )
 }
---
+++ src/widgets/header/ui/header.tsx
@@ -0,0 +1,9 @@
+import './header.css'
+
+export function Header() {
+    return(
+        <div className="header">
+            g
+        </div>
+    )
+}
