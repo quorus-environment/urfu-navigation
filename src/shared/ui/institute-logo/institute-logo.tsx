29d18e7ab6792153e85e90315d89a8c099665ad5
Konstantine <Konstaphy@gmail.com>
2022-10-05 17:25:19 +0500

    Резолв конфликтов после слияния с девом

--- src/shared/ui/institute-logo/institute-logo.tsx
+++ src/shared/ui/institute-logo/institute-logo.tsx
@@ -1,5 +1,5 @@
-import './institute-logo.css'
-import React from 'react'
+import "./institute-logo.css"
+import React from "react"
 
 type TInstituteLogoProps = {
   photo: string
@@ -11,11 +11,15 @@ type TInstituteLogoProps = {
  * Компонента принимает ссылку на лого института(photo) и его название(title)
  * ЯВляется кнопкой для каждого института
  * **/
-export const InstituteLogo: React.FC<TInstituteLogoProps> = ({ photo, title, onClick }) => {
+export const InstituteLogo: React.FC<TInstituteLogoProps> = ({
+  photo,
+  title,
+  onClick,
+}) => {
   return (
-    <div className='institute-logo' onClick={onClick}>
-      <img className='institute-logo__photo' src={photo} alt='' />
-      <span className='institute-logo__title'>{title}</span>
+    <div className="institute-logo" onClick={onClick}>
+      <img className="institute-logo__photo" src={photo} alt="" />
+      <span className="institute-logo__title">{title}</span>
     </div>
   )
 }
