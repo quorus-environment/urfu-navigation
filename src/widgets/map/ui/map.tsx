405936b455505b062eba6c450b100f15e2e7ca5d
lenaa1 <85735585+lenaa1@users.noreply.github.com>
2022-10-07 17:12:18 +0500

    РТФ ЧЕМПИОН (появилась иконка ртф на карте)

--- src/widgets/map/ui/map.tsx
+++ src/widgets/map/ui/map.tsx
@@ -1,4 +1,5 @@
-import { YMaps, Map } from "react-yandex-maps"
+import { YMaps, Map, Placemark } from "react-yandex-maps"
+import RTF from "../../../shared/assets/RTF.svg"
 
 export const MainMap = () => (
   <YMaps>
@@ -6,6 +7,16 @@ export const MainMap = () => (
       height="100%"
       width="100%"
       defaultState={{ center: [56.842862, 60.652578], zoom: 15 }}
-    />
+    >
+      <Placemark
+        options={{
+          iconLayout: "default#image",
+          iconImageHref: RTF,
+          iconImageSize: [64, 32],
+          iconImageOffset: [25, -25],
+        }}
+        geometry={[56.840508, 60.650206]}
+      />
+    </Map>
   </YMaps>
 )
