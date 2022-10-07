07baeebd255c3577c8b4cf814be9ded1d8aeea6a
Konstantine <Konstaphy@gmail.com>
2022-10-07 20:41:29 +0500

    feat: Навигация с меню

--- src/widgets/map/ui/map.tsx
+++ src/widgets/map/ui/map.tsx
@@ -1,22 +1,29 @@
 import { YMaps, Map, Placemark } from "react-yandex-maps"
 import RTF from "../../../shared/assets/RTF.svg"
+import { useNavigate } from "react-router-dom"
 
-export const MainMap = () => (
-  <YMaps>
-    <Map
-      height="100%"
-      width="100%"
-      defaultState={{ center: [56.842862, 60.652578], zoom: 15 }}
-    >
-      <Placemark
-        options={{
-          iconLayout: "default#image",
-          iconImageHref: RTF,
-          iconImageSize: [64, 32],
-          iconImageOffset: [25, -25],
-        }}
-        geometry={[56.840508, 60.650206]}
-      />
-    </Map>
-  </YMaps>
-)
+export const MainMap = () => {
+  const open = useNavigate()
+  return (
+    <YMaps>
+      <Map
+        height="100%"
+        width="100%"
+        defaultState={{ center: [56.842862, 60.652578], zoom: 15 }}
+      >
+        <Placemark
+          onClick={() => {
+            open("/irit-rtf")
+          }}
+          options={{
+            iconLayout: "default#image",
+            iconImageHref: RTF,
+            iconImageSize: [64, 32],
+            iconImageOffset: [25, -25],
+          }}
+          geometry={[56.840508, 60.650206]}
+        />
+      </Map>
+    </YMaps>
+  )
+}
