de71adcf521aa7ce5778976bb744246226671eab
Dmitry Ovechkin <ovechkin_dima03@mail.ru>
2022-10-13 18:38:02 +0500

    location refactored

--- src/shared/ui/location/location.tsx
+++ src/shared/ui/location/location.tsx
@@ -1,11 +1,15 @@
 import React, { FC } from "react"
+import { locationColor } from "../ui-constants/ui-constants"
 
 interface LocationProps {
   color?: string
   size?: number
 }
 
-export const Location: FC<LocationProps> = ({ color = "#9D9B98", size }) => {
+export const Location: FC<LocationProps> = ({
+  color = locationColor,
+  size,
+}) => {
   return (
     <svg
       width={size ? size : 14}
