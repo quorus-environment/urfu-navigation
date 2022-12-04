ac17da9f7d64f39e8356b33f1fc884f0dbb5f51c
Danil Gribanov <gribanovwork@yandex.ru>
2022-12-05 02:41:30 +0500

    Добавил комментарии

--- BreadthSearch.ts
+++ BreadthSearch.ts
@@ -8,8 +8,10 @@ export type TGraph = {
   id: string
   points: [number, number]
   direction: Side
-  isFilled?: boolean
   destination: GraphDestination
+  section: number
+  floor: number
+  isFilled?: boolean
   height?: number
   neighbors?: TGraph[]
 }
@@ -20,6 +22,8 @@ const ri101: TGraph = {
   id: "РИ-101",
   points: [425, 450],
   direction: Side.BOTTOM,
+  section: 1,
+  floor: 1,
   isFilled: false,
   destination: GraphDestination.AUDITORIUM,
   height: 25
@@ -29,6 +33,8 @@ const ri102: TGraph = {
   id: "РИ-102",
   points: [575, 450],
   direction: Side.BOTTOM,
+  section: 1,
+  floor: 1,
   isFilled: false,
   destination: GraphDestination.AUDITORIUM,
   height: 25
@@ -38,6 +44,8 @@ const ri103: TGraph = {
   id: "РИ-103",
   points: [725, 450],
   direction: Side.BOTTOM,
+  section: 1,
+  floor: 1,
   isFilled: false,
   destination: GraphDestination.AUDITORIUM,
   height: 25
@@ -47,6 +55,8 @@ const ri105: TGraph = {
   id: "РИ-105",
   points: [425, 500],
   direction: Side.TOP,
+  section: 1,
+  floor: 1,
   isFilled: false,
   destination: GraphDestination.AUDITORIUM,
   height: 25
@@ -56,6 +66,8 @@ const ri106: TGraph = {
   id: "РИ-106",
   points: [575, 500],
   direction: Side.TOP,
+  section: 1,
+  floor: 1,
   isFilled: false,
   destination: GraphDestination.AUDITORIUM,
   height: 25
@@ -65,6 +77,8 @@ const ri107: TGraph = {
   id: "РИ-107",
   points: [725, 500],
   direction: Side.TOP,
+  section: 1,
+  floor: 1,
   isFilled: false,
   destination: GraphDestination.AUDITORIUM,
   height: 25
@@ -74,6 +88,8 @@ const cor2: TGraph = {
   id: "cor2",
   points: [575, 475],
   direction: Side.RIGHT,
+  section: 1,
+  floor: 1,
   isFilled: false,
   destination: GraphDestination.CORRIDOR,
   height: 150,
@@ -83,6 +99,8 @@ const cor1: TGraph = {
   id: "cor1",
   points: [425, 475],
   direction: Side.RIGHT,
+  section: 1,
+  floor: 1,
   isFilled: false,
   destination: GraphDestination.CORRIDOR,
   height: 150,
@@ -102,16 +120,18 @@ ri107.neighbors = [cor2, ri103];
 console.log(findPaths(ri101, ri107));
 
 
+// Поиск в ширину от графа startGraph до графа endGraph
 function* createLinkedListPath(startGraph: TGraph, endGraph: TGraph): Generator<LinkedList<string> | undefined> {
   const queue = new Queue<TGraph>();
   queue.enqueue(startGraph);
-  const visited = new Set();
+  const visited = new Set(); // Сет для посещеных графов
   visited.add(startGraph);
-  const tracks = new Map<string, LinkedList<string>>;
-  const initLL = new LinkedList<string>(startGraph.id);
+  const tracks = new Map<string, LinkedList<string>>; // Словарь, связывающий айдишник со связанным списком
+  const initLL = new LinkedList<string>(startGraph.id); // Создаем голову связанного списка
   tracks.set(startGraph.id, initLL);
   while (queue.length != 0) {
     const graph = queue.dequeue();
+    // Добавляем всех соседедей graph в очередь
     for (let i = 0; i < (graph.neighbors?.length || 0); i++){
       const neighbor = graph.neighbors?.[i];
       if (!neighbor || visited.has(neighbor.id))
@@ -122,13 +142,13 @@ function* createLinkedListPath(startGraph: TGraph, endGraph: TGraph): Generator<
       tracks.set(neighbor.id, currentLL)
     }
   }
-
+  // Есди мы посетили конечный граф, то выводим генератор в виде связанного списка айдишников
   if (tracks.has(endGraph.id))
     yield tracks.get(endGraph.id);
 }
 
 
-
+// Разворачиваем связанный лист в массив айдишников
 function findPaths (startGraph: TGraph, endGraph: TGraph): string[] {
   const pathLinkedList = createLinkedListPath(startGraph, endGraph);
   const iterationResult = pathLinkedList.next();
