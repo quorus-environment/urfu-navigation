90b5eeaf4acedba4a8e4a6b98142c19714f3112e
Danil Gribanov <gribanovwork@yandex.ru>
2022-12-07 01:59:28 +0500

    Сделал 5 тестов для поиска пути

--- src/find-path.test.ts
+++ src/find-path.test.ts
@@ -1,12 +1,62 @@
 import "jest"
 import { TGraph } from "./entities/graph/model/interface"
-test("huy", () => {
-  expect(
-    (function () {
-      return 1 + 1
-    })(),
-  ).toBe(2)
+import { findPaths } from "./find-path"
+import { getGraphsFromAuditoriums } from "./entities/auditorium/lib/get-graphs-from-auditoriums"
+import { auditoriumsConfig, neighborsGraph } from "./pages/universities/config"
+
+const graph = [
+  ...neighborsGraph,
+  ...getGraphsFromAuditoriums(auditoriumsConfig),
+]
+
+test("FindPathInSectionWithoutCoridor", () => {
+  expect(findPaths("РИ-101", "РИ-105", graph)).toStrictEqual([
+    "РИ-101",
+    "РИ-105",
+  ])
 })
+
+test("FindPathInSectionWithOneCoridor", () => {
+  expect(findPaths("РИ-101", "РИ-106", graph)).toStrictEqual([
+    "РИ-101",
+    "cor_1",
+    "РИ-106",
+  ])
+})
+
+test("FindPathInSectionWithManyCoridors", () => {
+  expect(findPaths("РИ-101", "РИ-108", graph)).toStrictEqual([
+    "РИ-101",
+    "cor_1",
+    "cor_2",
+    "cor_3",
+    "РИ-108",
+  ])
+})
+
+test("FindPathInSectionToTurnOver", () => {
+  expect(findPaths("РИ-101", "turnover1to2", graph)).toStrictEqual([
+    "РИ-101",
+    "cor_1",
+    "cor_2",
+    "cor_3",
+    "turnover1to2",
+  ])
+})
+
+test("FindPathToOtherSection", () => {
+  expect(findPaths("РИ-101", "РИ-110", graph)).toStrictEqual([
+    "РИ-101",
+    "cor_1",
+    "cor_2",
+    "cor_3",
+    "turnover1to2",
+    "turnover2to1",
+    "cor_4",
+    "РИ-110",
+  ])
+})
+
 export type section = {
   sectionName: string
   graph: TGraph[]
