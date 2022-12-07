7a30282ef4195532ec162a234a619dd7231e9c78
Dmitry Ovechkin <ovechkin_dima03@mail.ru>
2022-12-07 23:42:34 +0500

    fix: useless imports

--- src/widgets/header/ui/header.tsx
+++ src/widgets/header/ui/header.tsx
@@ -1,11 +1,9 @@
 import "./header.css"
-import { OrdinarySearch } from "../../../shared/ordinarySearch/ordinarySearch"
 import { DoubleSearch } from "../../../shared/doubleSearch/doubleSearch"
 
 export const Header = () => {
   return (
     <div className="header">
-      {/* <OrdinarySearch /> */}
       <DoubleSearch />
     </div>
   )
