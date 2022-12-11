4ceb0dff1cb186cea1d43b06866d1316503e613c
Dmitry Ovechkin <ovechkin_dima03@mail.ru>
2022-12-11 23:52:33 +0500

    создал компоненту выбора этажа

---
+++ src/shared/ chooseFloor/chooseFloor.tsx
@@ -0,0 +1,36 @@
+import { FC, useState } from "react"
+import "./chooseFloor.css"
+import { NavLink } from "react-router-dom"
+
+interface IChooseFloor {
+  links: string[]
+  size?: number
+}
+/**Component of choosing floors
+ *
+ * @links string[] - links an array of paths to pages with floors
+ * @size number - a size of component
+ * */
+export const ChooseFloor: FC<IChooseFloor> = ({ links, size = 32 }) => {
+  const [activeIndex, setActiveIndex] = useState(-1)
+  const currentPath = window.location.pathname
+  const list = links.map((floor, index) => {
+    const linkHandler = () => setActiveIndex(index)
+    return (
+      <NavLink
+        style={{ width: size, height: size, fontSize: size * 0.6 }}
+        to={floor}
+        key={index}
+        className={
+          activeIndex === index || currentPath === floor
+            ? "floor active-link"
+            : "floor"
+        }
+        onClick={linkHandler}
+      >
+        {index}
+      </NavLink>
+    )
+  })
+  return <div className="floors-container">{list}</div>
+}
