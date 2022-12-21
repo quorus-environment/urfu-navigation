3534856c1f28a13860a88297c225feee42356b25
Konstantine <Konstaphy@gmail.com>
2022-12-21 23:17:10 +0500

    feat: доделал автокомплит

--- src/shared/ordinarySearch/ordinarySearch.tsx
+++ src/shared/ordinarySearch/ordinarySearch.tsx
@@ -1,6 +1,13 @@
 import "./ordinarySearch.css"
 import Lens from "../assets/Vector.svg"
-import { FC, useEffect, useMemo, useRef, useState } from "react"
+import {
+  FC,
+  MutableRefObject,
+  useEffect,
+  useMemo,
+  useRef,
+  useState,
+} from "react"
 
 const auditoriumsNames: string[] = [
   "РИ-101",
@@ -18,16 +25,12 @@ interface IOrdinarySearch {
   name: string
 }
 
-export const OrdinarySearch: FC<IOrdinarySearch> = ({ name }) => {
-  const [inputValue, setInputValue] = useState("")
-  const [isOpenedBar, setOpenedBar] = useState(true)
-
+const useAutocompleteClosingByRef = (setOpened: (v: boolean) => void) => {
   const ref = useRef<HTMLDivElement | null>(null)
-
   useEffect(() => {
     const handler = (event: Event) => {
       if (!ref.current?.contains(event.target as Node)) {
-        setOpenedBar(false)
+        setOpened(false)
       }
     }
     document.addEventListener("click", handler)
@@ -35,18 +38,89 @@ export const OrdinarySearch: FC<IOrdinarySearch> = ({ name }) => {
       document.removeEventListener("click", handler)
     }
   })
+  return ref
+}
 
+const useKeyboardManagement = (
+  ref: MutableRefObject<HTMLDivElement | null>,
+  filtered: string[],
+  setInputValue: (v: string) => void,
+  setOpenedBar: (v: boolean) => void,
+) => {
+  const [currentIndex, setCurrentIndex] = useState(-1)
+  useEffect(() => {
+    const current = ref.current
+    const handler = (evt: KeyboardEventInit) => {
+      console.log(evt.code)
+      if (evt.code === "ArrowUp") {
+        if (currentIndex === -1) {
+          return
+        }
+        setCurrentIndex(currentIndex - 1)
+      }
+      if (evt.code === "ArrowDown") {
+        if (currentIndex === filtered.length - 1) {
+          return
+        }
+        setCurrentIndex(currentIndex + 1)
+      }
+      if (evt.code === "Enter") {
+        setOpenedBar(false)
+        if (currentIndex !== -1) {
+          setInputValue(filtered[currentIndex])
+        }
+        setCurrentIndex(-1)
+      }
+    }
+    current?.addEventListener("keydown", handler)
+    return () => {
+      current?.removeEventListener("keydown", handler)
+    }
+  }, [
+    currentIndex,
+    filtered,
+    filtered.length,
+    ref,
+    setInputValue,
+    setOpenedBar,
+  ])
+  return currentIndex
+}
+
+export const OrdinarySearch: FC<IOrdinarySearch> = ({ name }) => {
+  const [inputValue, setInputValue] = useState("")
+  const [isOpenedBar, setOpenedBar] = useState(true)
+
+  // Закрытие модалки на клик извне
+  const ref = useAutocompleteClosingByRef(setOpenedBar)
+
+  // Получаем текущие строки в выпадающее меню
   const filtered = useMemo(() => {
-    return auditoriumsNames.filter((auditorium) =>
-      auditorium.toLowerCase().includes(inputValue.toLowerCase()),
-    )
+    if (!inputValue) {
+      return []
+    }
+    return auditoriumsNames
+      .filter((auditorium) =>
+        auditorium.toLowerCase().includes(inputValue.toLowerCase()),
+      )
+      .slice(0, 5)
   }, [inputValue])
+
+  // Выбор клавишами
+  const currentIndex = useKeyboardManagement(
+    ref,
+    filtered,
+    setInputValue,
+    setOpenedBar,
+  )
 
   const listItem = useMemo(() => {
     return filtered.map((name, index) => (
       <li
         key={index}
-        className="auto-complete__item"
+        className={`auto-complete__item ${
+          currentIndex === index ? " hovered" : undefined
+        }`}
         onClick={() => {
           setInputValue(name)
           setOpenedBar(false)
@@ -55,7 +129,7 @@ export const OrdinarySearch: FC<IOrdinarySearch> = ({ name }) => {
         {name}
       </li>
     ))
-  }, [filtered])
+  }, [currentIndex, filtered])
 
   return (
     <div className="body">
@@ -66,7 +140,11 @@ export const OrdinarySearch: FC<IOrdinarySearch> = ({ name }) => {
           type="text"
           placeholder={name}
           value={inputValue}
+          onKeyDown={(e) => {
+            e.stopPropagation()
+          }}
           onChange={(event) => {
+            setOpenedBar(true)
             setInputValue(event.target.value)
           }}
           onClick={() => setOpenedBar(true)}
