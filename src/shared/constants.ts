ce5897d6d9f4743f01909e8dec8d447f9e592a68
Danil Gribanov <gribanovwork@yandex.ru>
2023-01-16 19:34:48 +0500

    feat: прописал GraphDestination штукам и сделал розовый более розовым

--- src/shared/constants.ts
+++ src/shared/constants.ts
@@ -3,8 +3,8 @@ export enum Colors {
   LightYellow = "#ffff0030",
   Gray = "#00000030",
   LightOrange = "rgba(255,91,0,0.19)",
-  LightBlue = "rgba(0,197,255,0.31)",
-  LightPink = "rgba(255,0,0,0.25)",
+  LightBlue = "rgba(0,197,255,0.2)",
+  LightPink = "rgba(245,62,252,0.17)",
   LightGreen = "#12ff0030",
   LightGray = "rgba(49,49,49,0.10)",
 }
