276626a15a623eec29b0e264dbec190bc74d6d36
Konstantine <Konstaphy@gmail.com>
2023-01-16 19:51:47 +0500

    feat: обновил цвета

--- src/shared/constants.ts
+++ src/shared/constants.ts
@@ -2,7 +2,7 @@ export enum Colors {
   LightRed = "#ff000010",
   LightYellow = "#ffff0030",
   Gray = "#00000030",
-  LightOrange = "rgba(255,91,0,0.19)",
+  LightOrange = "rgba(255,145,0,0.2)",
   LightBlue = "rgba(0,197,255,0.2)",
   LightPink = "rgba(245,62,252,0.17)",
   LightGreen = "#12ff0030",
