547907a54927b6378495dd789cf7a71620f6ef25
Konstantine <Konstaphy@gmail.com>
2023-02-18 19:06:35 +0500

    feat: разделил тестирование и  поправил тест домашней страницы

--- cypress/e2e/app.cy.ts
+++ cypress/e2e/app.cy.ts
@@ -1,5 +1,11 @@
 
-it("homepage map exists", () => {
-  cy.visit("http://localhost:3000/")
-  cy.get("ymaps").should("exist")
+describe("homepage", () => {
+  it("homepage map exists", () => {
+    cy.visit("http://localhost:3000/")
+    cy.get("ymaps").should("exist")
+  })
+  it("homepage universities list exists", () => {
+    cy.visit("http://localhost:3000/")
+    cy.get(".main__institutes").should("have.length.greaterThan", 0)
+  })
 })
