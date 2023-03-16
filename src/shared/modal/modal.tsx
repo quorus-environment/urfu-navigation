07a7dff1fa01a2c47b90b03e095f459916a5fbc9
Dmitry Ovechkin <ovechkin_dima03@mail.ru>
2023-03-16 23:31:59 +0500

    feat: добавил возможность закрытия на escape

--- src/shared/modal/modal.tsx
+++ src/shared/modal/modal.tsx
@@ -1,4 +1,4 @@
-import React, { FC } from "react"
+import React, { FC, useEffect } from "react"
 import ModalOverlay from "../modalOverlay/modalOverlay"
 import { createPortal } from "react-dom"
 import styles from "./modal.module.css"
@@ -13,6 +13,12 @@ type TModal = {
   title: string
 }
 const Modal: FC<TModal> = ({ children, handleClose, title }) => {
+  useEffect(() => {
+    const escClosing: any = (e: React.KeyboardEvent) =>
+      e.key === "Escape" ? handleClose() : null
+    document.body.addEventListener("keydown", escClosing)
+    return () => document.body.removeEventListener("keydown", escClosing)
+  }, [handleClose])
   return createPortal(
     <>
       <ModalOverlay handleClose={handleClose} />
