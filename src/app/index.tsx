5a0793eebcb0baa90ef8c63beee745ce19b61a58
Konstantine <Konstaphy@gmail.com>
2023-05-26 15:22:52 +0500

    убрал лог

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -3,41 +3,12 @@ import { Routes, Route } from "react-router-dom"
 import { Main } from "../pages/main/main"
 import { IritRtf } from "../pages/universities/irit-rtf/irit-rtf"
 import "./app.css"
-import { findPathThroughSection } from "../shared/pathFinding/find-path-through-section"
 import { TestUniv } from "../pages/universities/test/test-univ"
 
 /**
  * Основная компонента приложения
- *
  * */
 export const App: React.FC = () => {
-  console.log(
-    findPathThroughSection(
-      { x: 0, y: 0 },
-      { x: 1, y: 3 },
-      {
-        transition: "vertical",
-        vectors: [
-          [
-            { x: 0, y: 0 },
-            { x: 1, y: 0 },
-          ],
-          [
-            { x: 1, y: 0 },
-            { x: 1, y: 4 },
-          ],
-          [
-            { x: 1, y: 4 },
-            { x: 0, y: 4 },
-          ],
-          [
-            { x: 0, y: 4 },
-            { x: 0, y: 0 },
-          ],
-        ],
-      },
-    ),
-  )
   return (
     <div className="app">
       <Routes>
